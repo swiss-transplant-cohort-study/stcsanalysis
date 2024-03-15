e8118c7af23920ffe8ce29996d6643ed65c1ac08
Jaromil Frossard <jaromil.frossard@usb.ch>
2024-03-15 10:15:33 +0100

    ties arg in which.pmin_chr()

--- R/which.pmin.R
+++ R/which.pmin.R
@@ -1,7 +1,8 @@
 #' Indicate the index of the row-wise minimal value
 #'
 #' @param ... Vectors of the same type where which.min is applied.
-#' @param silent a logical indicating if the function should return a warning in case of duplicated minimal.
+#' @param silent lgl. Indicate if the function should return a warning in case of duplicated minimal.
+#' @param ties chr. Method use in case of ties. Either \code{"warning-first"}, default which return a warning a select the name of the first input, OR \code{"first"}, return the first input, OR \code{"collapse"}, collapse the tied input with \code{" | "}.
 #'
 #' @return an integer vector indicating the row-wise index of the (first) minimal value
 #'
@@ -21,7 +22,7 @@ which.pmin <- function(...,silent = F){
           sum(x==min(x,na.rm=T),na.rm=T)>1
         }
     }),na.rm = T)){
-      warning("Duplicated minimum")
+      warning("Ties at minimum")
     }
   }
   unname(apply(cbind(...), 1, \(x){
@@ -32,13 +33,25 @@ which.pmin <- function(...,silent = F){
 #' @export
 #' @importFrom rlang dots_list
 #' @rdname whichp
-which.pmin_chr <- function(...,silent = F){
+which.pmin_chr <- function(...,ties = c("warning-first","first","collapse")){
+  ties <- match.arg(ties)
+
   dots <- dots_list(...,.named = T)
   vn <- names(dots)
-  vn[which.pmin(...,silent=silent)]
 
+  switch(ties,
+         "warning-first" ={vn[which.pmin(...,silent=F)]},
+         "first"= {vn[which.pmin(...,silent=T)]},
+         "collapse" = {
+           sapply(apply(cbind(...),1,\(x)which(x==noinf_min(x))),\(xi){
+             if(all(is.na(xi))){
+               NA_character_
+             }else{
+               paste(vn[xi],collapse = " | ")
+             }
+           })
+         })
 }
-
 
 
 
