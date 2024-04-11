febe4f7eb1f29175d2f733bf901ea296e059e7bd
Jaromil Frossard <jaromil.frossard@usb.ch>
2024-04-11 08:51:13 +0200

    add ethnicity patientbl

--- R/tailored_patientbl.R
+++ R/tailored_patientbl.R
@@ -14,11 +14,13 @@
 #' \item{3.} \code{enrollment_age}: Patient age at enrollment. Source: \code{Patient}.
 #' \item{4.} \code{sex}: Patient sex. Source: \code{Patient}.
 #' \item{5.} \code{bg}: Patient blood group. Source: \code{Patient}.
-#' \item{6.} \code{pre_tpx_count}: Number of pre-STSC transplantation. Source: \code{Patient}.
-#' \item{7.} \code{pre_organ_count}: Number of pre-STSC organ. Source: \code{Patient}.
-#' \item{8.} \code{weight}: Weight of patient at enrollment. Source: \code{PatientLongitudinal}.
-#' \item{9.} \code{height}: Height of patient at enrollment. Source: \code{PatientLongitudinal}.
-#' \item{10.-24.} \code{hla...}: (Optional) Patient HLA. Source: \code{PatientHLA}.
+#' \item{6.} \code{ethnicity}: Patient ethnicity. Source: \code{Patient}.
+#' \item{7.} \code{ethnicity_comment}: Comment on \code{patient_ethnicity}. Source: \code{Patient}.
+#' \item{8.} \code{pre_tpx_count}: Number of pre-STSC transplantation. Source: \code{Patient}.
+#' \item{9.} \code{pre_organ_count}: Number of pre-STSC organ. Source: \code{Patient}.
+#' \item{10.} \code{weight}: Weight of patient at enrollment. Source: \code{PatientLongitudinal}.
+#' \item{11.} \code{height}: Height of patient at enrollment. Source: \code{PatientLongitudinal}.
+#' \item{12.-26.} \code{hla...}: (Optional) Patient HLA. Source: \code{PatientHLA}.
 #' }
 #'
 #'@name tailored_table
@@ -29,7 +31,7 @@ tailored_patientbl <- function(stcs){
 
   out <-
     stcs[["patient"]] |>
-    select(all_of(c("patientkey","patid","enrollment_age","sex","bg","pre_tpx_count","pre_organ_count"))) |>
+    select(all_of(c("patientkey","patid","enrollment_age","sex","bg","ethnicity","ethnicity_comment","pre_tpx_count","pre_organ_count"))) |>
     left_join(
       stcs[["patientlongitudinal"]] |>
         filter(!!sym("type")=="bl") |>
